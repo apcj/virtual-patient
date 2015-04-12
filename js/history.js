a7f96c5c66f9b1a4a9ef6a81106cca58df14d77c
Alistair Jones <alistair.jones@gmail.com>
2015-04-12 11:35:07 +0100

    Drop words to have them appended to the notes.

--- js/history.js
+++ js/history.js
@@ -2,6 +2,10 @@
     var allWords = [];
     var selectedWords = [];
 
+    var notes = [];
+
+    var sectionNotes = d3.select('#section-notes');
+
     function updateSelectionClasses() {
         allWords.forEach(function(d) {
             d.selected = false;
@@ -13,6 +17,13 @@
         });
         d3.selectAll('#section-history span')
             .classed('selected', function(d) { return d.selected; });
+    }
+
+    function renderNotes() {
+        var paragraphs = sectionNotes.select('#list-notes').selectAll('p').data(notes);
+
+        paragraphs.enter().append('p')
+            .text(function(d) { return d.quotation; });
     }
 
     d3.selectAll('#section-history p').each(function() {
@@ -41,8 +52,8 @@
             var e = d3.event.sourceEvent;
             if (e.touches) {
                 return {
-                    x: e.touches[0].pageX,
-                    y: e.touches[0].pageY
+                    x: e.touches[0].clientX,
+                    y: e.touches[0].clientY
                 };
             }
             return {
@@ -66,10 +77,12 @@
             .on('drag', function() {
                 var position = computePointerPosition();
                 var hoverElement = document.elementFromPoint(position.x, position.y);
+                sectionNotes
+                    .classed('hover', draggingWords && sectionNotes.node().contains(hoverElement));
                 if (draggingWords) {
                     d3.selectAll('div.drag-words')
-                        .style('left', position.x + 'px')
-                        .style('top', position.y + 'px');
+                        .style('left', (position.x + 10) + 'px')
+                        .style('top',  (position.y + 10) + 'px');
                 } else {
                     var draggedElement = this;
                     function findSequence(direction, reverse) {
@@ -94,7 +107,16 @@
                 }
             })
             .on('dragend', function() {
-                d3.selectAll('div.drag-words').remove();
+                if (draggingWords && sectionNotes.classed('hover')) {
+                    var dragHandle = d3.selectAll('div.drag-words');
+                    var quotationText = dragHandle.text();
+                    dragHandle.remove();
+                    sectionNotes.classed('hover', false);
+                    notes.push({
+                        quotation: quotationText
+                    });
+                    renderNotes();
+                }
                 draggingWords = false;
             });
     });
